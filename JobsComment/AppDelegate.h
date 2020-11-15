ea07ba061bd2b19ccb9afb7bc6c2798d133fb1ca
Jobs <lg295060456@gmail.com>
2020-11-15 14:25:09 +0700

    Initial Commit

---
+++ JobsComment/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  JobsComment
+//
+//  Created by Jobs on 2020/11/15.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (readonly, strong) NSPersistentCloudKitContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
